b5c1e38439be524500f7e571be4bd1ba92d6cc02
soujitdas65 <soujitdas65@gmail.com>
2025-07-22 14:34:02 +0530

    Time: 6 ms (40.47%) | Memory: 53.3 MB (38.02%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,16 +1,12 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mpp=new HashMap<>();
-        int x=0;
-        for (int i=0;i<nums.length;i++){
-            int freq=0;
-            if (mpp.containsKey(nums[i])) freq=mpp.get(nums[i]);
-            if (freq+1>nums.length/2) {
-                x=nums[i];
-                break;
-            }
-            mpp.put(nums[i],freq+1);
+        Arrays.sort(nums);
+        int a=nums[0],count=1;
+        for (int i=1;i<nums.length;i++){
+            if(nums[i]==a) count++;
+            else a=nums[i];
+            if(count>nums.length/2) return a;
         }
-        return x;
+        return a;
     }
 }
