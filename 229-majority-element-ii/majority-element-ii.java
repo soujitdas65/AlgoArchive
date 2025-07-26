06845c43b108086aca1066c8e66e3c7d5ff105f4
soujitdas65 <soujitdas65@gmail.com>
2025-07-26 14:16:04 +0530

    Time: 2 ms (99.84%) | Memory: 50.9 MB (9.26%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,14 +1,32 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer,Integer> mpp=new HashMap<>();
-        List<Integer> list=new ArrayList<>();
+        int ele1=Integer.MIN_VALUE, ele2=Integer.MIN_VALUE;
         int l=nums.length;
+        int count1=0, count2=0;
         for(int i=0;i<l;i++){
-            int freq=0;
-            if(mpp.containsKey(nums[i])) freq=mpp.get(nums[i]);
-            if(freq+1>l/3 && !list.contains(nums[i])) list.add(nums[i]);
-            mpp.put(nums[i],freq+1);
+            if(count1==0 && ele2!=nums[i]){
+                ele1=nums[i];
+                count1++;
+            }
+            else if(count2==0 && ele1!=nums[i]){
+                ele2=nums[i];
+                count2++;
+            }
+            else if(ele1==nums[i]) count1++;
+            else if(ele2==nums[i]) count2++;
+            else {
+                count1--;
+                count2--;
+            }
         }
+        count1=0;count2=0;
+        List<Integer> list=new ArrayList<>();
+        for(int i=0;i<l;i++){
+            if(ele1==nums[i]) count1++;
+            if(ele2==nums[i]) count2++;
+        }
+        if(count1>(l/3)) list.add(ele1);
+        if(count2>(l/3)) list.add(ele2);
         return list;
     }
 }
