eed2d39fc53ef2fd7732938cb2719b1a620242e8
soujitdas65 <soujitdas65@gmail.com>
2025-07-29 08:40:57 +0530

    Time: 1 ms (29.04%) | Memory: 42.2 MB (78.47%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -3,6 +3,20 @@ class Solution {
         for (int i=0;i<n;i++) {
             nums1[m+i]=nums2[i];
         }
-        Arrays.sort(nums1);
+        int gap=(m+n)/2;
+        while(gap>0){
+            int left=0,right=left+gap;
+            while(right<m+n){
+                if(nums1[left]>nums1[right]){
+                    int temp=nums1[left];
+                    nums1[left]=nums1[right];
+                    nums1[right]=temp;
+                }
+                left++;
+                right++;
+            }
+            if(gap==1) break;
+            gap=gap/2+gap%2;
+        }
     }
 }
