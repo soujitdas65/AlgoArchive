2a682fc2157d3bb824d878d3b53ab6a09004d2f7
soujitdas65 <soujitdas65@gmail.com>
2025-08-05 16:32:17 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (30.13%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,24 +1,20 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int low = 0, high = nums.length;
-        int ans = -1;
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (mid % 2 == 1) {
-                if (mid>0 && nums[mid] == nums[mid - 1])
+        int low=0,high=nums.length;
+        int ans=-1;
+        while(low<=high) {
+            int mid=low+(high-low)/2;
+            if (mid%2==1) {
+                if (mid>0 && nums[mid]==nums[mid-1]) low=mid+1;
+                else if(mid<nums.length-1 && nums[mid]==nums[mid+1])  
+                    high=mid-1;
+                else return nums[mid];
+            } 
+            else{
+                if(mid>0 && nums[mid]==nums[mid-1]) high=mid-1;
+                else if(mid<nums.length-1 && nums[mid]==nums[mid+1])
                     low = mid + 1;
-                else if (mid<nums.length-1 && nums[mid] == nums[mid + 1])
-                    high = mid - 1;
-                else
-                    return nums[mid];
-                mid = low + (high - low) / 2;
-            } else {
-                if (mid>0 && nums[mid] == nums[mid - 1])
-                    high = mid - 1;
-                else if (mid<nums.length-1 && nums[mid] == nums[mid + 1])
-                    low = mid + 1;
-                else
-                    return nums[mid];
+                else return nums[mid];
             }
         }
         return ans;
