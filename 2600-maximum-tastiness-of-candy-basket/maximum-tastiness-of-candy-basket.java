557cbb208a01ed84f8c4bcd03963ce7549b13bf4
soujitdas65 <soujitdas65@gmail.com>
2025-08-10 14:09:29 +0530

    Time: 45 ms (12.73%) | Memory: 57.9 MB (46.73%) - LeetSync

--- 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
@@ -1,8 +1,12 @@
 class Solution {
     public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
         int l=price.length;
-        mergeSort(price,0,l-1);
-        int low=1,high=price[l-1]-price[0];
+        int low=Integer.MAX_VALUE,high=price[l-1]-price[0];
+        for(int i=1;i<l;i++){
+            int diff=price[i]-price[i-1];
+            low=Math.min(diff,low);
+        }
         while(low<=high){
             int mid=low+(high-low)/2;
             int last=price[0],count=1;
@@ -17,37 +21,5 @@ class Solution {
             else high=mid-1;
         }
         return high;
-    }
-    public int[] mergeSort(int ar[], int low, int high) {
-        if (low >= high)
-            return ar;
-        int mid = (low + high) / 2;
-        mergeSort(ar, low, mid);
-        mergeSort(ar, mid + 1, high);
-        return merge(ar, low, mid, high);
-    }
-    public int[] merge(int ar[], int low, int mid, int high) {
-        List<Integer> test = new ArrayList<>();
-        int left = low, right = mid+1;
-        while (left <= mid && right <= high) {
-            if (ar[left] < ar[right]) {
-                test.add(ar[left]);
-                left++;
-            } else {
-                test.add(ar[right]);
-                right++;
-            }
-        }
-        while (left <= mid) {
-            test.add(ar[left]);
-            left++;
-        }
-        while (right <= high) {
-            test.add(ar[right]);
-            right++;
-        }
-        for (int i = low; i <= high; i++)
-            ar[i] = test.get(i - low);
-        return ar;
     }
 }
