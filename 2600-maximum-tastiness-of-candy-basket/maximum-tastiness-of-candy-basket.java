db20d5d81fe5ac4921f56e536a3c5a2ba13cda7f
soujitdas65 <soujitdas65@gmail.com>
2025-08-10 14:03:36 +0530

    Time: 104 ms (5.82%) | Memory: 58.1 MB (23.45%) - LeetSync

--- 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maximumTastiness(int[] price, int k) {
-        Arrays.sort(price);
         int l=price.length;
+        mergeSort(price,0,l-1);
         int low=1,high=price[l-1]-price[0];
         while(low<=high){
             int mid=low+(high-low)/2;
@@ -17,5 +17,37 @@ class Solution {
             else high=mid-1;
         }
         return high;
+    }
+    public int[] mergeSort(int ar[], int low, int high) {
+        if (low >= high)
+            return ar;
+        int mid = (low + high) / 2;
+        mergeSort(ar, low, mid);
+        mergeSort(ar, mid + 1, high);
+        return merge(ar, low, mid, high);
+    }
+    public int[] merge(int ar[], int low, int mid, int high) {
+        List<Integer> test = new ArrayList<>();
+        int left = low, right = mid+1;
+        while (left <= mid && right <= high) {
+            if (ar[left] < ar[right]) {
+                test.add(ar[left]);
+                left++;
+            } else {
+                test.add(ar[right]);
+                right++;
+            }
+        }
+        while (left <= mid) {
+            test.add(ar[left]);
+            left++;
+        }
+        while (right <= high) {
+            test.add(ar[right]);
+            right++;
+        }
+        for (int i = low; i <= high; i++)
+            ar[i] = test.get(i - low);
+        return ar;
     }
 }
