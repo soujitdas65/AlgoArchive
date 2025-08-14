d3f9ca6ccbbd3c8f771dd676579beb8af7b71466
soujitdas65 <soujitdas65@gmail.com>
2025-08-14 14:30:49 +0530

    Time: 4 ms (77.29%) | Memory: 46.7 MB (39.01%) - LeetSync

--- 2737-row-with-maximum-ones/row-with-maximum-ones.java
+++ 2737-row-with-maximum-ones/row-with-maximum-ones.java
@@ -7,12 +7,14 @@ class Solution {
             for(int j=0;j<mat[0].length;j++){
                 if(mat[i][j]==1){
                     count++;
-                }                
+                }  
+                if(count==mat[0].length)break;              
             }  
             if(count>ans[1]) {
                 ans[1]=count;  
                 ans[0]=i;
-            }      
+            } 
+            if(count==mat[0].length)break;      
         }
         return ans;
     }
