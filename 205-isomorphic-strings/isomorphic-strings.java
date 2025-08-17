f28b77d4ed03446747d5183250d5118a14e58939
soujitdas65 <soujitdas65@gmail.com>
2025-08-17 13:26:40 +0530

    Time: 17 ms (34.34%) | Memory: 42.5 MB (70.90%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,18 +1,16 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        List<Character> front=new ArrayList<>();
-        List<Character> back=new ArrayList<>();
+        HashMap<Character,Integer> front=new HashMap<>();
+        HashMap<Character,Integer> back=new HashMap<>();
         for(int i=0;i<s.length();i++){
-            if (front.contains(s.charAt(i))) {
-                if(back.get(front.indexOf(s.charAt(i)))!=t.charAt(i)) 
-                return false;
+            if (!front.containsKey(s.charAt(i))) {
+                front.put(s.charAt(i),i);
             }
-            if (back.contains(t.charAt(i))) {
-                if(front.get(back.indexOf(t.charAt(i)))!=s.charAt(i)) 
-                return false;
+            if (!back.containsKey(t.charAt(i))) {
+                back.put(t.charAt(i),i);
             }
-            front.add(s.charAt(i));
-            back.add(t.charAt(i));
+            if(!front.get(s.charAt(i)).equals(back.get(t.charAt(i))))
+            return false;
         }
         return true;
     }
