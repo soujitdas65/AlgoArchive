64c87fc87183c8640a8ed50c6e35ae8823a13301
soujitdas65 <soujitdas65@gmail.com>
2025-08-17 19:12:33 +0530

    Time: 6 ms (42.35%) | Memory: 43.3 MB (66.52%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character,Integer> mpp=new HashMap<>();
+        int num[]=new int[26];
         if(s.length()!=t.length()) return false;
         for(int i=0;i<s.length();i++){
-            int freq1=0,freq2=0;
-            if(mpp.containsKey(s.charAt(i)))
-            freq1=mpp.get(s.charAt(i));
-            mpp.put(s.charAt(i),freq1+1);
-            if(mpp.containsKey(t.charAt(i)))
-            freq2=mpp.get(t.charAt(i));
-            mpp.put(t.charAt(i),freq2-1);
+            num[s.charAt(i)-'a']++;
+            num[t.charAt(i)-'a']--;
         }
-        for(char ch:mpp.keySet()) {
-            if(mpp.get(ch)!=0) return false;
+        for(int i=0;i<26;i++){
+            if(num[i]!=0) return false;
         }
         return true;
     }
