1055a6a093af419e7b396e795709e9d761a9ad2e
soujitdas65 <soujitdas65@gmail.com>
2025-08-18 20:46:25 +0530

    Time: 3 ms (79.70%) | Memory: 44.7 MB (68.51%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,20 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
+        int[] roman = new int[26]; 
+        roman['I'-'A'] = 1;
+        roman['V'-'A'] = 5;
+        roman['X'-'A'] = 10;
+        roman['L'-'A'] = 50;
+        roman['C'-'A'] = 100;
+        roman['D'-'A'] = 500;
+        roman['M'-'A'] = 1000;
+
         int sum=0;
         for(int i=0;i<s.length()-1;i++){
-            if(roman(s.charAt(i))<roman(s.charAt(i+1)))
-            sum-=roman(s.charAt(i));
-            else sum+=roman(s.charAt(i));
+            if(roman[s.charAt(i)-'A']<roman[s.charAt(i+1)-'A'])
+            sum-=roman[s.charAt(i)-'A'];
+            else sum+=roman[s.charAt(i)-'A'];
         }
-        return sum+roman(s.charAt(s.length()-1));
-    }
-    int roman(char ch){
-        if(ch=='I') return 1;
-        if(ch=='V') return 5;
-        if(ch=='X') return 10;
-        if(ch=='L') return 50;
-        if(ch=='C') return 100;
-        if(ch=='D') return 500;
-        return 1000;
+        return sum+roman[s.charAt(s.length()-1)-'A'];
     }
 }
