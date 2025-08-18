18be4546f0b3dfd40de5f8ca1961f6620a73fc1e
soujitdas65 <soujitdas65@gmail.com>
2025-08-18 20:36:22 +0530

    Time: 3 ms (79.70%) | Memory: 45.2 MB (14.23%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,20 +1,20 @@
 class Solution {
     public int romanToInt(String s) {
-        HashMap<Character,Integer> roman=new HashMap<>();
-        roman.put('I',1);
-        roman.put('V',5);
-        roman.put('X',10);
-        roman.put('L',50);
-        roman.put('C',100);
-        roman.put('D',500);
-        roman.put('M',1000);
-
         int sum=0;
         for(int i=0;i<s.length()-1;i++){
-            if(roman.get(s.charAt(i))<roman.get(s.charAt(i+1)))
-            sum-=roman.get(s.charAt(i));
-            else sum+=roman.get(s.charAt(i));
+            if(roman(s.charAt(i))<roman(s.charAt(i+1)))
+            sum-=roman(s.charAt(i));
+            else sum+=roman(s.charAt(i));
         }
-        return sum+roman.get(s.charAt(s.length()-1));
+        return sum+roman(s.charAt(s.length()-1));
+    }
+    int roman(char ch){
+        if(ch=='I') return 1;
+        if(ch=='V') return 5;
+        if(ch=='X') return 10;
+        if(ch=='L') return 50;
+        if(ch=='C') return 100;
+        if(ch=='D') return 500;
+        return 1000;
     }
 }
