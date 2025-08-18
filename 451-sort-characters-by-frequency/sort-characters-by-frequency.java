db1e7216ca75125354b76d74700dcfc80781a6df
soujitdas65 <soujitdas65@gmail.com>
2025-08-18 16:46:57 +0530

    Time: 81 ms (9.70%) | Memory: 45.9 MB (41.66%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,17 +1,14 @@
 class Solution {
     public String frequencySort(String s) {
         HashMap<Character,Integer> mpp=new HashMap<>();
-        for(int i=0;i<s.length();i++) {
-          int freq=0;
-          if(mpp.containsKey(s.charAt(i))) freq=mpp.get(s.charAt(i));
-          mpp.put(s.charAt(i),freq+1);
-        }
+        for(char ch:s.toCharArray()) 
+        mpp.put(ch,mpp.getOrDefault(ch,0)+1);
         StringBuilder ans=new StringBuilder();
-        int i=s.length();
-        while(i-->0){
+        int l=s.length();
+        while(l-->0){
             for(char ele:mpp.keySet()){
                 int n=mpp.get(ele);
-                if(n==i+1){
+                if(n==l+1){
                     while(n-->0) ans.append(ele);
                 }
             }
