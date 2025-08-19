e72f2d0b8c91a50332a0db11a27737ea611b85bc
soujitdas65 <soujitdas65@gmail.com>
2025-08-19 08:51:54 +0530

    Time: 2 ms (24.25%) | Memory: 42.9 MB (14.95%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.java
+++ 8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,30 +1,28 @@
 class Solution {
     public int myAtoi(String s) {
         if(s.length()==0) return 0;
-        StringBuilder sign=new StringBuilder();
-        StringBuilder ans=new StringBuilder();
+        StringBuilder str=new StringBuilder();
         int i=0;
         while(i<s.length() && s.charAt(i)==' ')i++;
-        if(i>=s.length()) return 0;
-        if(s.charAt(i)=='+' || s.charAt(i)=='-') 
-        sign.append(s.charAt(i++));
-        if(i>=s.length()) return 0;
-        while(s.charAt(i)>='0' && s.charAt(i)<='9'){
-            ans.append(s.charAt(i++));
-            if(i>=s.length()) break;
+        if(i<s.length() && (s.charAt(i)=='-' || s.charAt(i)=='+'))
+        str.append(s.charAt(i++));
+        boolean blank=true;
+        while(i<s.length()){
+            if(s.charAt(i)>='0' && s.charAt(i)<='9'){
+                str.append(s.charAt(i++));
+                blank=false;
+            }
+            else break;
         }
-        if(ans.length()==0) return 0;
-        String numStr = sign.toString() + ans.toString();
-        long num;
-        try {
-            num = Long.parseLong(numStr); // use long to detect overflow
-        } catch (NumberFormatException e) {
-            return sign.toString().equals("-") ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+        if(blank==true) return 0;
+        int result;
+        try{
+            result=Integer.valueOf(str.toString());
+        }catch(Exception e){
+            if(str.length()!=0 && str.charAt(0)=='-') 
+            return Integer.MIN_VALUE;
+            else return Integer.MAX_VALUE;
         }
-
-        // 6. Clamp to int range
-        if (num > Integer.MAX_VALUE) return Integer.MAX_VALUE;
-        if (num < Integer.MIN_VALUE) return Integer.MIN_VALUE;
-        return (int)num;
+        return result;
     }
 }
