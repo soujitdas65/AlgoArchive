04501af5a038d21188955fc5fec4c86cb63ff083
soujitdas65 <soujitdas65@gmail.com>
2025-08-23 22:38:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (40.95%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,18 +1,17 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int m=matrix.length;
-        int n=matrix[0].length;
+        int n=matrix.length, m=matrix[0].length;
         //rotate
-        for (int i=0;i<m/2;i++){
-            for (int j=0;j<n;j++){
+        for(int i=0;i<m/2;i++){
+            for(int j=0;j<n;j++){
                 int temp=matrix[i][j];
-                matrix[i][j]=matrix[m-1-i][j];
-                matrix[m-1-i][j]=temp;
+                matrix[i][j]=matrix[m-i-1][j];
+                matrix[m-i-1][j]=temp;
             }
         }
         //transpose
-        for (int i=0;i<m;i++){
-            for (int j=i;j<n;j++){
+        for(int i=0;i<m;i++){
+            for(int j=i;j<n;j++){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
@@ -20,3 +19,4 @@ class Solution {
         }
     }
 }
+    
