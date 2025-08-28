618b743a7e36590ee42ee8730c0ef366d6fba87d
soujitdas65 <soujitdas65@gmail.com>
2025-08-28 22:25:38 +0530

    Time: 9 ms (96.13%) | Memory: 57.3 MB (22.25%) - LeetSync

--- LinkedList/148-sort-list/sort-list.java
+++ LinkedList/148-sort-list/sort-list.java
@@ -10,20 +10,37 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        List<Integer> list=new ArrayList<>();
-        //storingElements
-        ListNode temp=head;
-        while(temp!=null) {
-            list.add(temp.val);
-            temp=temp.next;
+        if(head==null || head.next==null) return head;
+        ListNode mid=middle(head), tail=mid.next;
+        mid.next=null;
+        head=sortList(head);
+        tail=sortList(tail);
+        return merge(head, tail);
+    }
+    ListNode middle(ListNode head) {
+        ListNode slow=head, fast=head.next.next;
+        while(fast!=null && fast.next!=null) {
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        Collections.sort(list);
-        //updateList
-        temp=head;
-        for(int i:list) {
-            temp.val=i;
-            temp=temp.next;
+        return slow;
+    }
+    ListNode merge(ListNode head, ListNode tail) {
+        ListNode dummy=new ListNode(Integer.MAX_VALUE) ,temp=dummy;
+        while(head!=null && tail!=null) {
+            if(head.val<=tail.val) {
+                temp.next=head;
+                temp=temp.next;
+                head=head.next;
+            }
+            else {
+                temp.next=tail;
+                temp=temp.next;
+                tail=tail.next;
+            }
         }
-        return head;
+        if(head!=null) temp.next=head;
+        else temp.next=tail;
+        return dummy.next;
     }
 }
