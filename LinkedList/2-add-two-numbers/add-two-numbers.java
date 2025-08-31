9ac1df0b70fdccf0cb235b32cb269a734c689a1b
soujitdas65 <soujitdas65@gmail.com>
2025-08-31 07:14:14 +0530

    Time: 1 ms (100.00%) | Memory: 44.7 MB (29.27%) - LeetSync

--- LinkedList/2-add-two-numbers/add-two-numbers.java
+++ LinkedList/2-add-two-numbers/add-two-numbers.java
@@ -10,33 +10,20 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(length(l1)<length(l2)) return addTwoNumbers(l2,l1);
-        ListNode temp1=l1, temp2=l2,prev=null;
+        ListNode dummy=new ListNode(-1);
+        ListNode head1=l1, head2=l2, curr=dummy;
         int carry=0;
-        while(temp1!=null && temp2!=null) {
-            int sum=temp1.val+temp2.val+carry;
-            temp1.val=sum%10;
+        while(head1!=null || head2!=null) {
+            int sum=carry;
+            if(head1!=null) sum+=head1.val;
+            if(head2!=null) sum+=head2.val;
+            curr.next=new ListNode(sum%10);
+            curr=curr.next;
             carry=sum/10;
-            prev=temp1;
-            temp1=temp1.next;
-            temp2=temp2.next;
+            if(head1!=null) head1=head1.next;
+            if(head2!=null) head2=head2.next;
         }
-        while(temp1!=null) {
-            int sum=temp1.val+carry;
-            temp1.val=sum%10;
-            carry=sum/10;
-            prev=temp1;
-            temp1=temp1.next; 
-        }
-        if(carry!=0) prev.next=new ListNode(carry); 
-        return l1;
-    }
-    int length(ListNode temp) {
-        int cnt=0;
-        while(temp!=null) {
-            cnt++;
-            temp=temp.next;
-        }
-        return cnt;
+        if(carry!=0) curr.next=new ListNode(carry);
+        return dummy.next;
     }
 }
