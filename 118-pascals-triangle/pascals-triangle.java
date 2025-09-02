3138b381ba635aec58e11dc1127758dcb5ca2bfd
soujitdas65 <soujitdas65@gmail.com>
2025-09-02 17:39:25 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (24.91%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -1,20 +1,17 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result=new ArrayList<>();
-        for(int i=0;i<numRows;i++){
-            result.add(generateRows(i));
-        }  
-        return result;
-    }
-    List<Integer> generateRows(int rowIndex){
-        long ans=1;
-        List<Integer> list=new ArrayList<>();
-        list.add((int)ans);
-        for(int i=0;i<rowIndex;i++){
-            ans*=rowIndex-i;
-            ans/=i+1;
-            list.add((int)ans);
+        List<List<Integer>> mainList=new ArrayList<>();
+        for(int i=0;i<numRows;i++) {
+            List<Integer> list=new ArrayList<>();
+            int ans=1;
+            list.add(ans);
+            for(int j=0;j<i;j++) {
+                ans*=(i-j);
+                ans/=(j+1);
+                list.add(ans);
+            }
+            mainList.add(list);
         }
-        return list;
+        return mainList;
     }
 }
