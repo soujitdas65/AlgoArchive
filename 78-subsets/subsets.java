b2abd7587b64aea2f9c992ae5961b14c9358f00e
soujitdas65 <soujitdas65@gmail.com>
2025-09-05 17:05:57 +0530

    Time: 1 ms (48.65%) | Memory: 42 MB (99.94%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -6,12 +6,13 @@ class Solution {
         return result;
     }
     void generate(int i,int[] nums,Stack<Integer> st,List<List<Integer>> result) {
-        result.add(new ArrayList<>(st));
-        while(i<nums.length) {  
-            st.push(nums[i]);
-            generate(i+1,nums,st,result);
-            st.pop();
-            i++;
+        if(i==nums.length){
+            result.add(new ArrayList<>(st));
+            return;
         }
+        generate(i+1,nums,st,result);//Choice:skip
+        st.push(nums[i]);//Choice:choose
+        generate(i+1,nums,st,result);
+        st.pop();
     }
 }
