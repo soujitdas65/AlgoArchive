309d75cdc7b0eb6f09670ce84ef57712961b070c
soujitdas65 <soujitdas65@gmail.com>
2025-09-06 12:10:28 +0530

    Time: 5 ms (7.65%) | Memory: 44.9 MB (20.15%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -2,21 +2,23 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> result=new ArrayList<>();
         Stack<Integer> st=new Stack<>();
-        find(result,st,0,candidates,0,target);
+        find(result,st,0,candidates,target);
         return result;
     }
-    void find(List<List<Integer>> result, Stack<Integer> st, int i, int[] nums, int sum, int target) {
-        if(i>=nums.length || sum>target) {
-            if(sum==target) result.add(new ArrayList<>(st));
+    void find(List<List<Integer>> result, Stack<Integer> st, int i, int[] nums, int target) {
+        if(i>=nums.length) {
+            if(target==0) result.add(new ArrayList<>(st));
             return;
         }
         //skip 
-        find(result,st,i+1,nums,sum,target);
+        find(result,st,i+1,nums,target);
         //include 
-        sum+=nums[i];
-        st.push(nums[i]);
-        find(result,st,i,nums,sum,target);
-        sum-=nums[i];
-        st.pop();
+        if(target>=0) {
+            target-=nums[i];
+            st.push(nums[i]);
+            find(result,st,i,nums,target);
+            target+=nums[i];
+            st.pop();
+        }
     }
 }
