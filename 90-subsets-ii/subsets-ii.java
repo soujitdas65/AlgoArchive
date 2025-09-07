b348a3cdf0e27454afd405b10fa1b032e0628074
soujitdas65 <soujitdas65@gmail.com>
2025-09-07 15:22:08 +0530

    Time: 1 ms (99.95%) | Memory: 43.5 MB (79.38%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -5,13 +5,17 @@ class Solution {
         findSubset(result,new Stack<>(),0,nums);
         return result;
     }
-    void findSubset(List<List<Integer>> result,Stack<Integer> st,int start,int[] nums ) {
-        result.add(new ArrayList<>(st));
-        for(int i=start;i<nums.length;i++) {
-            if(i>start && nums[i]==nums[i-1]) continue;
-            st.push(nums[i]);
-            findSubset(result,st,i+1,nums);
-            st.pop();
+    void findSubset(List<List<Integer>> result,Stack<Integer> st,int i,int[] nums ) {
+        if(i>=nums.length) {
+            result.add(new ArrayList<>(st));
+            return;
         }
+        //include 
+        st.push(nums[i]);
+        findSubset(result,st,i+1,nums);
+        st.pop();
+        //skippingDuplicates
+        while(i+1<nums.length && nums[i]==nums[i+1]) i++;
+        findSubset(result,st,i+1,nums);
     }
 }
