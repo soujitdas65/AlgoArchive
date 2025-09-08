abea319eb6651ade3643f25ce5caf843b22d5684
soujitdas65 <soujitdas65@gmail.com>
2025-09-08 07:40:24 +0530

    Time: 1 ms (17.12%) | Memory: 40.7 MB (96.83%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.java
+++ 216-combination-sum-iii/combination-sum-iii.java
@@ -4,17 +4,17 @@ class Solution {
         findSum(result,new Stack<>(),1,k,n);
         return result;
     }
-    void findSum(List<List<Integer>> result,Stack<Integer> st,int start,int k,int n) {
+    void findSum(List<List<Integer>> result,Stack<Integer> st,int i,int k,int n) {
         if(k==0 && n==0) {
             result.add(new ArrayList<>(st));
             return;
         }
-        for(int i=start;i<10;i++) {
-            if(n<=0 || k<=0) return;
-            //include
-            st.push(i);
-            findSum(result,st,i+1,k-1,n-i);
-            st.pop();
-        }
+        if(i>=10 || k<0 || n<0) return;
+        //include
+        st.push(i);
+        findSum(result,st,i+1,k-1,n-i);
+        st.pop();
+        //skip
+        findSum(result,st,i+1,k,n);
     }
 }
