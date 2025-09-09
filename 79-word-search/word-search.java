937e07db430095ede7f8e396699daa84f6b24d07
soujitdas65 <soujitdas65@gmail.com>
2025-09-09 16:49:44 +0530

    Time: 131 ms (81.50%) | Memory: 42.1 MB (23.33%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        //iterateThroughtTheMatrix
+        for(int i=0;i<board.length;i++) {
+            for(int j=0;j<board[0].length;j++) {
+                if(wordSearch(board,i,j,word,0)==true) return true;
+            }
+        }
+        return false;
+    }
+    boolean wordSearch(char[][]board,int i,int j,String word,int k) {
+        if(k==word.length()) return true;
+        if(word.length()==1 && board[i][j]==word.charAt(k)) return true;
+        boolean flag=false;
+        if(word.charAt(k)==board[i][j]){
+            char temp=board[i][j];
+            board[i][j]=' ';
+            if(i>=1) flag=flag || wordSearch(board,i-1,j,word,k+1);
+            if(j>=1) flag=flag || wordSearch(board,i,j-1,word,k+1);
+            if(i<board.length-1) flag=flag || wordSearch(board,i+1,j,word,k+1);
+            if(j<board[0].length-1) flag=flag || wordSearch(board,i,j+1,word,k+1);
+            board[i][j]=temp;
+        }
+        return flag;
+    }
+}
