f9200a215d93385a6d22f50f7706517c09a8d8ca
soujitdas65 <soujitdas65@gmail.com>
2025-09-11 08:29:33 +0530

    Time: 2 ms (85.57%) | Memory: 44.8 MB (89.51%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,52 +1,50 @@
 class Solution {
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> nQueen=new ArrayList<>();
-        String[][] board=new String[n][n];
+        char[][] board=new char[n][n];
         for(int i=0;i<n;i++) {
             for(int j=0;j<n;j++) {
-                board[i][j]=".";
+                board[i][j]='.';
             }
         }
         placeQueen(nQueen,0,board,n);
         return nQueen;
     }
-    void placeQueen(List<List<String>> nQueen,int j,String[][] board,int n) {
+    void placeQueen(List<List<String>> nQueen,int j,char[][] board,int n) {
         if(j==n) {
             List<String> temp=new ArrayList<>();
             for(int i=0;i<n;i++) {
-                StringBuilder str=new StringBuilder();
-                for(int k=0;k<n;k++) str.append(board[i][k]);
-                temp.add(str.toString());
+                temp.add(new String(board[i]));
             }
             nQueen.add(temp);
             return;
         }
         for(int i=0;i<n;i++) {
             if(isQueenSafe(i,j,board)) {
-                board[i][j]="Q";
+                board[i][j]='Q';
                 placeQueen(nQueen,j+1,board,n);
-                board[i][j]=".";
+                board[i][j]='.';
             }
         }
     }
-    boolean isQueenSafe(int row,int col,String[][] board) {
+    boolean isQueenSafe(int row,int col,char[][] board) {
         int i=row;
         int j=col;
         while(i>=0 && j>=0) {
-            if(board[i][j]=="Q") return false;
+            if(board[i][j]=='Q') return false;
             i--;
             j--;
         }
         i=row;
         j=col;
         while(j>=0) {
-            if(board[i][j]=="Q") return false;
+            if(board[i][j]=='Q') return false;
             j--;
         }
         i=row;
         j=col;
         while(i<board.length && j>=0) {
-            if(board[i][j]=="Q") return false;
+            if(board[i][j]=='Q') return false;
             i++;
             j--;
         }
