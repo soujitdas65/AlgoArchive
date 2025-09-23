88db478b9598ad3bdaa3204a85fcacabb14203c5
soujitdas65 <soujitdas65@gmail.com>
2025-09-23 22:58:02 +0530

    Time: 70 ms (41.20%) | Memory: 52.5 MB (26.31%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,24 +1,23 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        int[] next=new int[arr.length];
-        int[] prev=new int[arr.length];
+        int n=arr.length;
+        int[] next=new int[n];
+        int[] prev=new int[n];
         Stack<Integer> nse=new Stack<>();
         Stack<Integer> pse=new Stack<>();
-        for(int i=arr.length-1;i>=0;i--) {
-            while(!nse.empty() && arr[nse.peek()]>=arr[i]) nse.pop();
-            next[i]=(nse.empty()) ? arr.length : nse.peek();
-            nse.push(i);
-        }
         for(int i=0;i<arr.length;i++) {
+            while(!nse.empty() && arr[nse.peek()]>=arr[n-i-1]) nse.pop();
             while(!pse.empty() && arr[pse.peek()]>arr[i]) pse.pop();
+            next[n-i-1]=(nse.empty()) ? n : nse.peek();
             prev[i]=(pse.empty()) ? -1 : pse.peek();
+            nse.push(n-i-1);
             pse.push(i);
         }
         long total=0,mod=1000000007;
         for(int i=0;i<arr.length;i++) {
             long a=i-prev[i];
             long b=next[i]-i;
-            total=(total+(((a*b)%mod)*arr[i])%mod)%mod;
+            total=(total+a*b%mod*arr[i]%mod)%mod;
         }
         return (int)(total%mod);
     }
