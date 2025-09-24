0e532abf46e2e2fbf231969957b4a5805b12c998
soujitdas65 <soujitdas65@gmail.com>
2025-09-24 18:05:56 +0530

    Time: 24 ms (46.11%) | Memory: 45 MB (14.39%) - LeetSync

--- 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
+++ 2227-sum-of-subarray-ranges/sum-of-subarray-ranges.java
@@ -1,14 +1,39 @@
 class Solution {
     public long subArrayRanges(int[] nums) {
-        long total=0;
-        for(int i=0;i<nums.length;i++) {
-            long min=Integer.MAX_VALUE,max=Integer.MIN_VALUE;
-            for(int j=i;j<nums.length;j++) {
-                max=Math.max(max,nums[j]);
-                min=Math.min(min,nums[j]);
-                total+=max-min;
-            }
+        int len=nums.length;
+        int[] nse=new int[len];
+        Stack<Integer> s1=new Stack<>();
+        for(int i=len-1;i>=0;i--) {
+            while(!s1.empty() && nums[s1.peek()]>=nums[i]) s1.pop();
+            nse[i]=(s1.empty()) ? len:s1.peek();
+            s1.push(i);
         }
-        return total;
+        int[] pse=new int[len];
+        Stack<Integer> s2=new Stack<>();
+        for(int i=0;i<len;i++) {
+            while(!s2.empty() && nums[s2.peek()]>nums[i]) s2.pop();
+            pse[i]=(s2.empty()) ? -1:s2.peek();
+            s2.push(i);
+        }
+        int[] nge=new int[len];
+        Stack<Integer> s3=new Stack<>();
+        for(int i=len-1;i>=0;i--) {
+            while(!s3.empty() && nums[s3.peek()]<=nums[i]) s3.pop();
+            nge[i]=(s3.empty()) ? len:s3.peek();
+            s3.push(i);
+        }
+        int[] pge=new int[len];
+        Stack<Integer> s4=new Stack<>();
+        for(int i=0;i<len;i++) {
+            while(!s4.empty() && nums[s4.peek()]<nums[i]) s4.pop();
+            pge[i]=(s4.empty()) ? -1:s4.peek();
+            s4.push(i);
+        }
+        long largeSum=0,smallSum=0;
+        for(int i=0;i<len;i++) {
+            largeSum+=(long)(i-pge[i])*(nge[i]-i)*nums[i];
+            smallSum+=(long)(i-pse[i])*(nse[i]-i)*nums[i];
+        }
+        return largeSum-smallSum;
     }
 }
