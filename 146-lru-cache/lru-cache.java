1d920d377c0a4f7e457a3be516787bb2db58dd00
soujitdas65 <soujitdas65@gmail.com>
2025-10-08 16:07:14 +0530

    Time: 43 ms (84.00%) | Memory: 113.2 MB (65.07%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,66 @@
+class LRUCache {
+    class Node {
+        int key,value;
+        Node next,prev;
+        Node(int key, int value) {
+            this.key=key;
+            this.value=value;
+            this.next=null;
+            this.prev=null;
+        }
+    }
+    HashMap<Integer,Node> mpp=new HashMap<>();
+    Node head=new Node(-1,-1);
+    Node tail=new Node(-1,-1);
+    int capacity;
+    public LRUCache(int capacity) {
+        this.capacity=capacity;
+        head.next=tail;
+        tail.prev=head;
+    }
+    public int get(int key) {
+        if(!mpp.containsKey(key)) return -1;
+        Node node=mpp.get(key);
+        deleteNode(node);
+        insertNode(node);
+        return node.value;
+    }
+    public void put(int key, int value) {
+        if(mpp.containsKey(key)) {
+            Node node=mpp.get(key);
+            deleteNode(node);
+            insertNode(node);
+            node.value=value;
+        }
+        else {
+            if(mpp.size()==capacity) {
+                Node node=tail.prev;
+                mpp.remove(node.key);
+                deleteNode(node);
+            }
+            Node node=new Node(key,value);
+            insertNode(node);
+            mpp.put(key,node);
+        }
+    }
+    void insertNode(Node node) {
+        Node temp=head.next;
+        node.next=temp;
+        node.prev=head;
+        head.next=node;
+        temp.prev=node;
+    }
+    void deleteNode(Node node) {
+        Node front=node.next;
+        Node back=node.prev;
+        if(back!=null) back.next=front;
+        if(front!=null) front.prev=back;
+    }
+}
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
