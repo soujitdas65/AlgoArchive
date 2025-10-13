e50228d4665084787c01a20521968cff65dfae9b
soujitdas65 <soujitdas65@gmail.com>
2025-10-13 21:11:53 +0530

    Time: 47 ms (5.97%) | Memory: 45.2 MB (12.32%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -4,27 +4,17 @@ class Solution {
     }
     int character(String s,int k) {
         HashMap<Character,Integer> mpp=new HashMap<>();
-        mpp.put('a',0);
-        mpp.put('b',0);
-        mpp.put('c',0);
         int i=0,j=0,cnt=0;
         while(j<s.length()) {
-            mpp.put(s.charAt(j),mpp.get(s.charAt(j))+1);
-            while(valid(mpp,k)) {
+            mpp.put(s.charAt(j),mpp.getOrDefault(s.charAt(j),0)+1);
+            while(mpp.size()>k) {
                 mpp.put(s.charAt(i),mpp.get(s.charAt(i))-1);
+                if(mpp.get(s.charAt(i))==0) mpp.remove(s.charAt(i));
                 i++;
             }
             cnt+=j-i+1;
             j++;
         }
         return cnt;
-    }
-    boolean valid(HashMap<Character,Integer> mpp,int k) {
-        int cnt=0;
-        for(char ch:mpp.keySet()) {
-            if(mpp.get(ch)>0) cnt++;
-        }
-        if(cnt<=k) return false;
-        return true;
     }
 }
