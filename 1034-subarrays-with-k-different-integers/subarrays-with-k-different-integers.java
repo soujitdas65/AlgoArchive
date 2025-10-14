4f9c3cb4789cbbd128666511907ee4facffbca13
soujitdas65 <soujitdas65@gmail.com>
2025-10-14 17:03:06 +0530

    Time: 48 ms (68.35%) | Memory: 48.2 MB (6.53%) - LeetSync

---
+++ 1034-subarrays-with-k-different-integers/subarrays-with-k-different-integers.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        return goodSubArray(nums,k)-goodSubArray(nums,k-1);
+    }
+    int goodSubArray(int[] nums, int k) {
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+        int i=0,j=0,cnt=0;
+        while(j<nums.length) {
+            mpp.put(nums[j],mpp.getOrDefault(nums[j],0)+1);
+            while(mpp.size()>k) {
+                mpp.put(nums[i],mpp.get(nums[i])-1);
+                if(mpp.get(nums[i])==0) mpp.remove(nums[i]);
+                i++;
+            }
+            cnt+=j-i+1;
+            j++;
+        }
+        return cnt;
+    }
+}
