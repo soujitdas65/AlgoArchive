81be6d6247426a6bea023947271cd3057f09000d
soujitdas65 <soujitdas65@gmail.com>
2025-10-22 18:06:19 +0530

    Time: 234 ms (19.70%) | Memory: 62.3 MB (19.85%) - LeetSync

--- 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
+++ 1422-divide-array-in-sets-of-k-consecutive-numbers/divide-array-in-sets-of-k-consecutive-numbers.java
@@ -4,14 +4,11 @@ class Solution {
         TreeMap<Integer,Integer> mpp=new TreeMap<>();
         for(int i:nums) mpp.put(i,mpp.getOrDefault(i,0)+1);
         while(!mpp.isEmpty()) {
-            int ele=mpp.firstKey();
-            mpp.put(ele,mpp.get(ele)-1);
-            if(mpp.get(ele)==0) mpp.remove(ele);
-            for(int i=1;i<k;i++) {
-                if(!mpp.containsKey(ele+1)) return false;
-                ele++;
-                mpp.put(ele,mpp.get(ele)-1);
-                if(mpp.get(ele)==0) mpp.remove(ele);
+            int i=mpp.firstKey();
+            for(int j=i;j<i+k;j++) {
+                if(!mpp.containsKey(j)) return false;  
+                mpp.put(j,mpp.get(j)-1);
+                if(mpp.get(j)==0) mpp.remove(j);           
             }
         }
         return true;
