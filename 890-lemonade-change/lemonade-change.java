0527c317ab6a12407e9579c47cb7e199747cdbbf
soujitdas65 <soujitdas65@gmail.com>
2025-10-28 15:45:44 +0530

    Time: 2 ms (93.34%) | Memory: 56.5 MB (7.08%) - LeetSync

--- 890-lemonade-change/lemonade-change.java
+++ 890-lemonade-change/lemonade-change.java
@@ -3,18 +3,20 @@ class Solution {
         int five=0,ten=0;
         for(int i=0;i<bills.length;i++) {
             if(bills[i]==5) five++;
-            else if(bills[i]==10) {
-                five--;
-                ten++;
-            }
             else {
-                if(ten>0) {
+                if(bills[i]==10) {
                     five--;
-                    ten--;
+                    ten++;
                 }
-                else five-=3;
+                else {
+                    if(ten>0) {
+                        five--;
+                        ten--;
+                    }
+                    else five-=3;
+                }
+                if(five<0) return false;
             }
-            if((five<0 || ten<0)) return false;
         }
         return true;
     }
